2410ab7d29374cba3ebd58ba96c41b378e900baa
antoniodeca88 <78661681+antoniodeca88@users.noreply.github.com>
2025-09-20 17:47:58 -0600

    Last Changes

--- swagger.js
+++ swagger.js
@@ -1,16 +1,21 @@
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 
-const options = {
+
+
+const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Contacts API',
       version: '1.0.0',
+      description: 'API to manage a list of contacts',
     },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
   },
-  apis: ['./routes/*.js'], // Documentación en comentarios
+  apis: ['./routes/*.js'],
 };
-
-const swaggerSpec = swaggerJsdoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
